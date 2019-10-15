8f7c523b241f4888ce8dab4d6b1e9f384e4f60e6
gaozx95 <602753653@qq.com>
2019-10-15 17:24:55 +0800

    Netty-server

--- src/main/java/netty/Server.java
+++ src/main/java/netty/Server.java
@@ -1,11 +1,13 @@
 package netty;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 
 public class Server {
     public static void main(String[] args) {
@@ -41,6 +43,19 @@ class ServerChildHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            System.out.println(new String(bytes));
+//            System.out.println(buf);
+            System.out.println(buf.refCnt());
+        }finally {
+            if(buf != null){
+                ReferenceCountUtil.release(buf);        //释放内存
+            }
+            System.out.println(buf.refCnt());
+        }
     }
 }
