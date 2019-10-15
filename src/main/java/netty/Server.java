73831d136702d734f3adadc479eae9c0139629b0
gaozx95 <602753653@qq.com>
2019-10-15 21:44:47 +0800

    Netty-server实现转发，server定义一个通道组channelgroup，server每次收到一个消息，就会向通道组里所有的通道发送msg

--- src/main/java/netty/Server.java
+++ src/main/java/netty/Server.java
@@ -3,11 +3,17 @@ package netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
-public class Server2 {
+public class Server {
+    //静态通道组 ，将所有连接（channelActive，通道可以用的时候）的channel加入到通道组
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);    //对通道组（clients）上的事件进行处理的线程（池）
+
     public static void main(String[] args) {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup(2);
@@ -28,6 +34,8 @@ public class Server2 {
                     .sync();
             System.out.println("server started!");
             f.channel().closeFuture().sync();       //调用close()方法会触发closeFuture
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }finally {
@@ -37,26 +45,34 @@ public class Server2 {
     }
 }
 
-class ServerChildHandler2 extends ChannelInboundHandlerAdapter {
+class ServerChildHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = null;
         try {
             buf = (ByteBuf) msg;
+
             byte[] bytes = new byte[buf.readableBytes()];
             buf.getBytes(buf.readerIndex(),bytes);
             System.out.println(new String(bytes));
 
+            Server.clients.writeAndFlush(msg);          //将通道组里所有的通道依次拿出来写数据
 
-            ctx.writeAndFlush(buf);
-//            System.out.println(buf);
-//            System.out.println(buf.refCnt());
         }finally {
 //            if(buf != null){
 //                ReferenceCountUtil.release(buf);        //释放内存
 //            }
-//            System.out.println(buf.refCnt());
         }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();        //执行完close后client端会触发closeFuture，从而结束client端
     }
 }
