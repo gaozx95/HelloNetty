be137f21ba87d2e92c43363c6829f174fcb62db7
gaozx95 <602753653@qq.com>
2019-10-15 10:13:20 +0800

    Initial commit

---
+++ src/main/java/io/bio/Client.java
@@ -0,0 +1,23 @@
+package io.bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("127.0.0.1", 8888);
+        s.getOutputStream().write("HelloServer".getBytes());
+        s.getOutputStream().flush();
+        //s.getOutputStream().close();
+        System.out.println("write over, waiting for msg back...");
+        byte[] bytes = new byte[1024];
+        int len = s.getInputStream().read(bytes);
+        System.out.println(new String(bytes, 0, len));
+        s.close();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+    }
+}
